d19c3d4c0618af350ea9bc88e70193235eb1429b
Nouredine Ouhaddou <nouhaddo@e1r13p1.1337.ma>
2020-01-28 12:15:38 +0100

    document error handlers

--- api/errors_view.py
+++ api/errors_view.py
@@ -2,7 +2,7 @@
 
 from api import *
 
-
+# handel page not found error
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({
@@ -11,7 +11,7 @@ def page_not_found(e):
                     "documentation_url"    : "%sdocs" %(request.host_url)
                     }), 404
 
-
+# handel rate limit exceeded error
 @app.errorhandler(429)
 def ratelimit_exceed(e):
     return jsonify({
@@ -20,12 +20,13 @@ def ratelimit_exceed(e):
                     "message"               : "You exceed rate limit for this entrypoint check documentation for more info about rate limit",
                     "documentation_url"     : "%sdocs" %(request.host_url)
                     }), 429
-    
+
+# handel methods  error
 @app.errorhandler(405)
 def method_error(e):
     return jsonify({
                     "status"                : "405",
-                    "title"                 : "Method %s not allowed",
+                    "title"                 : "Method not allowed",
                     "message"               : "the api accept only GET method check docs for more info",
                     "documentation_url"     : "%sdocs" %(request.host_url)
                     }), 405
