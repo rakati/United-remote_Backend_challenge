7f2a92a892952154bb1164d5e77731c1be525c78
Nouredine Ouhaddou <nouhaddo@e1r13p1.1337.ma>
2020-01-28 12:04:52 +0100

    add caching to routes

--- api/main.py
+++ api/main.py
@@ -1,74 +1,90 @@
 from api import *
+import urllib.parse
 
+# Override the default key generation
+# this for url with query string
+def cache_key():
+   args = request.args
+   key = request.path + '?' + urllib.parse.urlencode([
+     (k, v) for k in sorted(args) for v in sorted(args.getlist(k))
+   ])
+   return key
 
 # General inform that user may need
-@app.route('/')
-@limiter.limit("2 per minute")
+@app.route('/', methods = ['GET'])
 def general_msg():
 	return jsonify({
-		"trending_github_languages_url"		: "%s/languages" %(request.base_url),
-		"trending_github_repositories_url"	: "%s/repositories" %(request.base_url),
-		"trending_github_devlopers_url"		: "%s/developers/{language}{?since, daily, weekly, monthly}" %(request.base_url),
-		"trending_github_rate_limit_url"	: "%s/rate_limit" %(request.base_url),
-		"documentation_url"					: "%s/docs" %(request.base_url)
+		"trending_github_languages_url"		: "%slanguages" %(request.base_url),
+		"trending_github_repositories_url"	: "%srepositories" %(request.base_url),
+		"trending_github_devlopers_url"		: "%sdevelopers/{language}{?since, daily, weekly, monthly}" %(request.base_url),
+		"documentation_url"					: "%sdocs" %(request.host_url)
 	}), 200
 
 
 # return list of languages in 100 trending repo on github based on number of
 # stars and forks of the project each languages with list of repos created in it
-@app.route('/languages')
+@app.route('/languages', methods = ['GET'])
+@cache.cached()
 def laguages_list():
 	return jsonify(langs.trending_languages()), 200, limiter.header_mapping
 
 
 # return trending developer
-@app.route('/developers')
+@app.route('/developers', methods = ['GET'])
+@cache.cached(key_prefix=cache_key)
 def trending_developers():
 	range = request.args['since'] if 'since' in request.args else 'daily'
 	# check range validity
 	if range not in ['daily', 'weekly', 'monthly']:
 		return jsonify({
-					"status": "422",
-					"title":  "Invalid Attribute",
-					"detail": "range must be one of ['daily', 'weekly', 'monthly']"
+					"status"				: "422",
+					"title"					: "Invalid Attribute",
+					"detail"				: "range must be one of ['daily', 'weekly', 'monthly']",
+					"documentation_url"		: "%sdocs" %(request.host_url)
 				}), 422
 	return jsonify(developers.trending_developers(range=range)), 200
 
 
-@app.route('/developers/<lang>')
+@app.route('/developers/<lang>', methods = ['GET'])
+@cache.cached(key_prefix=cache_key)
 def trending_developers_lang(lang):
 	range = request.args['since'] if 'since' in request.args else 'daily'
 	# check range validity
 	if range not in ['daily', 'weekly', 'monthly']:
 		return jsonify({
-					"status": "422",
-					"title":  "Invalid Attribute",
-					"detail": "range must be one of ['daily', 'weekly', 'monthly']"
+					"status"				: "422",
+					"title"					: "Invalid Attribute",
+					"detail"				: "range must be one of ['daily', 'weekly', 'monthly']",
+					"documentation_url"		: "%sdocs" %(request.host_url)
 				}), 422
 	return jsonify(developers.trending_developers(language=lang, range=range,)), 200
-
-
-@app.route('/rate_limit')
-def famous_developer():
-	# return information about rate limit : rateLimit_remaining,
-	# rateLimit_limit
-	return "rate limit"
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify({
-					"status": "404",
-					"title":  "Page Not Found",
-					"documentation_url": "localhost:8100/docs"
+					"status"				: "404",
+					"title"					: "Page Not Found",
+					"documentation_url"		: "%sdocs" %(request.host_url)
 				}), 404
 
 
 @app.errorhandler(429)
-def ratelimit_handler(e):
+def ratelimit_exceed(e):
     return jsonify(
 		{
-			"status"	:	"429",
-			"title"		:	"Ratelimit exceeded",
-			"message"	:	"You exceed rate limit for this entrypoint check documentation for more info about rate limit"
+			"status"						: "429",
+			"title"							: "Ratelimit exceeded",
+			"message"						: "You exceed rate limit for this entrypoint check documentation for more info about rate limit",
+			"documentation_url"				: "%sdocs" %(request.host_url)
 		}), 429
+	
+@app.errorhandler(405)
+def method_error(e):
+	return jsonify(
+		{
+			"status"						: "405",
+			"title"							: "Method %s not allowed",
+			"message"						: "the api accept only GET method check docs for more info",
+			"documentation_url"				: "%sdocs" %(request.host_url)
+		}), 405
